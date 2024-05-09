6ded3e76031b7094875d782509606b286acaf62d
Hamed Arghavan <hamedaravane@gmail.com>
2024-05-09 16:11:51 -0700

    fix(orders): fix routes
    Signed-off-by: Hamed Arghavan <hamedaravane@gmail.com>

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -6,27 +6,26 @@ export const routes: Routes = [
     loadComponent: () => import('./layout.component').then(c => c.LayoutComponent),
     children: [
       {
-        path: '',
-        pathMatch: 'full',
-        loadComponent: () => import('../orders/orders.component').then(c => c.OrdersComponent),
-        data: {
-          title: 'Orders'
-        }
-      },
-      {
-        path: 'market',
-        loadComponent: () => import('../market/market.component').then(c => c.MarketComponent),
-        data: {
-          title: 'Market'
-        }
-      },
-      {
         path: 'arbitrage',
         loadComponent: () => import('../arbitrage/component/arbitrage.component').then(c => c.ArbitrageComponent),
         data: {
           title: 'Arbitrage'
         }
       },
+      {
+        path: 'orders',
+        loadComponent: () => import('../orders/orders.component').then(c => c.OrdersComponent),
+        data: {
+          title: 'Orders'
+        }
+      },
+      /*{
+        path: 'market',
+        loadComponent: () => import('../market/market.component').then(c => c.MarketComponent),
+        data: {
+          title: 'Market'
+        }
+      },*/
     ]
   }
 ];
