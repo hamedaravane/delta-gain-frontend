0b8d927e269e4f6f0513290e5515ed91b31cc2b6
Hamed Arghavan <hamedaravane@gmail.com>
2024-05-11 15:27:06 -0700

    feat(core): desktop & mobile components
    Signed-off-by: Hamed Arghavan <hamedaravane@gmail.com>

--- src/app/shared/components/desktop/desktop.component.ts
+++ src/app/shared/components/desktop/desktop.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-desktop',
+  selector: 'desktop',
   standalone: true,
   imports: [],
   template: `<ng-content></ng-content>`,
